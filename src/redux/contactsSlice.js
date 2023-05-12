f72a927d62c583afde48af2afae94b543268c3b9
Diana Forostiana <dianaforost@gmail.com>
2023-05-12 11:47:30 +0200

    Done

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,18 +1,23 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 const contactsInitialState = [
-    {id: 'id-1', userName: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', userName: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', userName: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', userName: 'Annie Copeland', number: '227-91-26'}];
+  {id: 'id-1', userName: 'Rosie Simpson', number: '459-12-56'},
+  {id: 'id-2', userName: 'Hermione Kline', number: '443-89-12'},
+  {id: 'id-3', userName: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-4', userName: 'Annie Copeland', number: '227-91-26'}
+];
     const contactsSlice = createSlice({
         name: "contacts",
-        initialState: contactsInitialState,
+        initialState: {
+          0: contactsInitialState
+        },
         reducers : {
             addContact: {
                 reducer(state, action) {
-                  console.log(state, action.payload);
-                  state.push(action.payload)
+                  state[0] = [...state[0], action.payload];
+                  // state[0].push(action.payload)
                 },
                 prepare(number, userName) {
                   return {
@@ -25,14 +30,20 @@ const contactsInitialState = [
                 },
               },
             deleteContact(state, action) {
-                const index = state.findIndex(contact => contact.id === action.payload)
-                state.splice(index, 1);
-                console.log(action.payload);
+                // const index = state.findIndex(contact => contact.id === action.payload)
+                // state.splice(index, 1);
+                state[0] = state[0].filter(task => task.id !== action.payload);
             }
         }
         
     })
-
-      
+    const persistConfig = {
+      key: 'contacts-initial-state',
+      storage,
+      whitelist: ["0"],
+    }
+    
+    
     export const { addContact, deleteContact } = contactsSlice.actions;
     export const contactsReducer =  contactsSlice.reducer;
+    export const persistedReducer = persistReducer(persistConfig, contactsReducer )
